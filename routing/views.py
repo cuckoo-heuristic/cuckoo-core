6f2c76ed63f24db22fdc226ed8f5aaa39f981b88
sara afzali <sarafzali83@gmail.com>
2025-11-27 22:30:07 +0330

    api

--- routing/views.py
+++ routing/views.py
@@ -9,12 +9,13 @@ class RouteAPIView(APIView):
         vehicle_id = request.data.get("vehicle_id")
 
         if not vehicle_id:
-            return Response({"error": "id?"})
+            return Response({"error": "id?"}, status=400)
+
         try:
             vehicle = Vehicle.objects.get(id=vehicle_id)
         except Vehicle.DoesNotExist:
-            return Response({"error": "ماشین پیدا نشد!"})
-        
+            return Response({"error": "The vehicle was not found!"}, status=404)
+
         origin_lat = vehicle.origin_lat
         origin_lon = vehicle.origin_lon
         dest_lat = vehicle.destination_lat
@@ -28,5 +29,21 @@ class RouteAPIView(APIView):
             "origin": f"{origin_lat},{origin_lon}",
             "destination": f"{dest_lat},{dest_lon}"
         }
+
         response = requests.get(url, headers=headers, params=params)
-        return Response(response.json())
+        data = response.json()
+
+        if "routes" not in data:
+            return Response({"error": "مسیر پیدا نشد"}, status=400)
+
+        formatted_routes = []
+
+        for route in data["routes"]:
+            formatted_routes.append({
+                "summary": route["legs"][0].get("summary", ""),
+                "distance": route["legs"][0]["distance"]["text"],
+                "duration": route["legs"][0]["duration"]["text"],
+                "polyline": route["overview_polyline"]["points"]
+            })
+
+        return Response({"routes": formatted_routes})
